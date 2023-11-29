6258b1dcfa58097b2f81c663b1d0f080d30f00a7
Thomas Goorden <thomas.goorden@gmail.com>
2023-11-29 13:51:40 +0100

    Upgrade eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -1,25 +1,15 @@
-const md = require('markdown-it')({
-  html: true,
-  linkify: true,
-  typographer: true
-})
-const markdownItAttrs = require('markdown-it-attrs')
-
-module.exports = async function (eleventyConfig) {
-  eleventyConfig.addFilter('md', function (content) {
-    md.use(markdownItAttrs)
-    return md.render(content)
-  })
-
-  eleventyConfig.addFilter('readableDate', function (content) {
-    const result = content.split('T')
-    const date = result[0].split('-')
-    return `${date[2]}/${date[1]}/${date[0]} ${result[1]}`
-  })
-
-  eleventyConfig.addPassthroughCopy('assets')
-
-  return {
-    passthroughFileCopy: true
-  }
+module.exports = function (eleventyConfig) {                                                                                                                                                                                                  
+    eleventyConfig.setLiquidOptions({                                                                                                                                                                                                         
+        dynamicPartials: false                                                                                                                                                                                                                
+    })                                                                                                                                                                                                                                        
+                                                                                                                                                                                                                                              
+    eleventyConfig.addFilter('readableDate', function (content) {                                                                                                                                                                             
+        const result = content.split('T')                                                                                                                                                                                                     
+        const date = result[0].split('-')                                                                                                                                                                                                     
+        return `${date[2]}/${date[1]}/${date[0]} ${result[1]}`                                                                                                                                                                                
+    })                                                                                                                                                                                                                                        
+                                                                                                                                                                                                                                              
+    return {                                                                                                                                                                                                                                  
+        passthroughFileCopy: true                                                                                                                                                                                                             
+    }                                                                                                                                                                                                                                         
 }
