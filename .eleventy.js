80a4b24a9f4ff9ff1dcf56f2075e752275fa205d
Thomas Goorden <thomas@goorden.be>
2023-11-29 13:57:45 +0100

    Upgrade Eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,15 @@
-module.exports = function (eleventyConfig) {                                                                                                                                                                                                  
-    eleventyConfig.setLiquidOptions({                                                                                                                                                                                                         
-        dynamicPartials: false                                                                                                                                                                                                                
-    })                                                                                                                                                                                                                                        
-                                                                                                                                                                                                                                              
-    eleventyConfig.addFilter('readableDate', function (content) {                                                                                                                                                                             
-        const result = content.split('T')                                                                                                                                                                                                     
-        const date = result[0].split('-')                                                                                                                                                                                                     
-        return `${date[2]}/${date[1]}/${date[0]} ${result[1]}`                                                                                                                                                                                
-    })                                                                                                                                                                                                                                        
-                                                                                                                                                                                                                                              
-    return {                                                                                                                                                                                                                                  
-        passthroughFileCopy: true                                                                                                                                                                                                             
-    }                                                                                                                                                                                                                                         
+module.exports = function (eleventyConfig) {
+    eleventyConfig.setLiquidOptions({
+        dynamicPartials: false
+    })
+
+    eleventyConfig.addFilter('readableDate', function (content) {
+        const result = content.split('T')
+        const date = result[0].split('-')
+        return `${date[2]}/${date[1]}/${date[0]} ${result[1]}`
+    })
+
+    return {
+        passthroughFileCopy: true
+    }
 }
